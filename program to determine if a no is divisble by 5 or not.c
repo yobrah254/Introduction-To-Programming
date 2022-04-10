9f28b530e833db305aaeb75c8f6fde7fc93af482
yobrah254 <102515745+yobrah254@users.noreply.github.com>
2022-04-10 23:40:07 +0300

    Add files via upload

---
+++ program to determine if a no is divisble by 5 or not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+  printf("Enter the no.");
+  scanf("%d",&a);
+  if(a%5==0)
+  {
+    printf("no is divisible by 5");
+  }
+  else
+    printf("no is not divisible by 5");
+
+    return 0;
+}
